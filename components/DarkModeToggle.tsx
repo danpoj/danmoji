fddb260f93fcd3c3c69d9dc6e787ebd2f2b4cb8b
danpoj <jws970306@khu.ac.kr>
2023-02-22 11:43:48 +0900

    fix: initial page !== hydrated page error

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -10,7 +10,22 @@ export default function DarkModeToggle() {
   }, [])
 
   if (!mounted) {
-    return <div className='h-10'></div>
+    return (
+      <div className='flex items-center gap-2 justify-end'>
+        <span className='text-sm text-slate-900 dark:text-slate-300 font-bold'>
+          ☀️
+        </span>
+
+        <label className='relative inline-flex items-center cursor-pointer'>
+          <input
+            checked={theme === 'dark'}
+            type='checkbox'
+            className='sr-only peer'
+          />
+          <div className="w-11 h-6 bg-slate-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-700 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-700"></div>
+        </label>
+      </div>
+    )
   }
 
   const onDarkModeToggleClicked = () =>
